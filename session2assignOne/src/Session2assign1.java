394394bc3b60e5a854cd0bac0fc19c808ff53c54
VrushaliB25 <vrbhoite@gmail.com>
2017-03-26 18:44:47 +0530

    Core Java Session2 Assignment1
    Code

---
+++ session2assignOne/src/Session2assign1.java
@@ -0,0 +1,22 @@
+
+public class Session2assign1 {
+	public static void main(String s[])
+	{
+		//To write a program to print the “X” pattern.
+
+		int i, j;
+			for (i=0; i<5;i++)
+			{
+				for (j=0;j<5;j++)
+				{
+					if (i==j || i+j==4)
+						System.out.print("*");
+					else 
+						System.out.print(" ");
+				}
+			System.out.println("");
+			}
+			
+		}
+		}
+
